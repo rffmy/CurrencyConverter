fdc45e7f5164274531c1643454fbe16d1c365565
rselivanov <rselivanov@cmtam.com>
2022-04-21 22:04:25 +0200

    JetBrains Academy: Currency Converter + Topics
    Work on project. Stage 6/6: Last but not least in Currency Converter
    completed 4 days ago 40

---
+++ Topics/Scopes/Hero damage/main.py
@@ -0,0 +1,16 @@
+hero_damage = 100
+
+def double_damage():
+    # pass
+    global hero_damage
+    hero_damage *= 2
+
+def disarmed():
+    # pass
+    global hero_damage
+    hero_damage *= 0.1
+
+def power_potion():
+    # pass
+    global hero_damage
+    hero_damage += 100
